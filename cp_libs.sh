415e473234ba2a9c5e947f79a51244b7788836fd
mraasvel <mbraasveldt@gmail.com>
2020-12-19 23:16:02 +0100

    automatic push

--- cp_libs.sh
+++ cp_libs.sh
@@ -1,4 +1,24 @@
+# Program to copy libs to directory from where you called this
+#
+#
+
 DIR=~/work/tools/
+
+declare -a LIBDIRS
+declare -a LIBS
+declare -a HEADERS
+
+LIBDIRS[0]=~/work/tools/libft/
+LIBS[0]=${LIBDIRS[0]}'libft.a'
+HEADERS[0]=${LIBDIRS[0]}'libft.h'
+
+LIBDIRS[1]=~/work/tools/vectorC/
+LIBS[1]=${LIBDIRS[1]}'libvect.a'
+HEADERS[1]=${LIBDIRS[1]}'libvect.h'
+
+# LIBDIRS[2]=~/work/tools/ft_printf/
+# LIBS[2]=${LIBDIRS[2]}'libftprintf.a'
+# HEADERS[2]=${LIBDIRS[2]}'ft_printf.h'
 
 if [[ $1 == "re" ]]
 then
@@ -8,16 +28,9 @@ else
 	CMD='make -C'
 fi
 
-echo $dir
-$CMD $DIR'libft'
-$CMD $DIR'vectorC'
-
-cp $DIR'libft/libft.a' $DIR
-cp $DIR'vectorC/libvect.a' $DIR
-cp $DIR'vectorC/libvect.h' .
-cp $DIR'libft/libft.h' .
-
-mv $DIR'libft.a' .
-mv $DIR'libvect.a' .
-
-# cp $DIR/cp_libs.sh .
+for i in $(seq 0 $(expr "${#LIBDIRS[@]}" - 1))
+do
+	$CMD ${LIBDIRS[$i]}
+	cp ${LIBS[$i]} .
+	cp ${HEADERS[$i]} .
+done
