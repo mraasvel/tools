255120ad5432f4e4bdc34fcddf55f2004ff1908f
Maarten benjamin Raasveldt <mraasvel@f0r5s7.codam.nl>
2021-03-24 17:39:37 +0100

    automatic push

--- libft/test.c
+++ libft/test.c
@@ -6,7 +6,7 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/19 10:52:25 by mraasvel      #+#    #+#                 */
-/*   Updated: 2021/03/18 14:56:37 by mraasvel      ########   odam.nl         */
+/*   Updated: 2021/03/24 16:07:51 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,24 @@
 
 long long int	ft_strtoll(const char *nptr);
 
+void	test(void)
+{
+	int	i;
+	char	*str = "12'abcd'12345";
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'')
+			i = ft_strchr(str + i + 1, '\'') - str + 1;
+		printf("%c", str[i]);
+		i++;
+	}
+	printf("\n");
+}
+
 int	main(int argc, char **argv)
 {
-	long long int	num;
-	char			*str;
-
-	str = "9223372036854775807";
-	num = ft_strtoll(str);
-	// num = strtoll(str, NULL, 0);
-	printf("%lld\n", num);
-	printf("ERRNO: %d : %d\n", errno, ERANGE);
+	test();
 	return (0);
 }
